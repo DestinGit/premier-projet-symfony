a0c735f33a57077c9179ef434a1fefbf8988a376
formation <bak1tino@gmail.com>
2017-08-29 11:53:21 +0200

    Passage des variable depuis le controller. TWIG : Variables composite, filtres, Tags, Boucle et variables, Tests Inclusion des parties du codes html.twig

--- src/AppBundle/Controller/CalculatorController.php
+++ src/AppBundle/Controller/CalculatorController.php
@@ -16,11 +16,24 @@ class CalculatorController extends Controller
      */
     public function addAction($n1, $n2)
     {
+
+        $clients = [
+            ['nom' => 'Alfred', 'id'=>5, 'email'=>'alfred@client.com', 'sexe'=>'M'],
+            ['nom' => 'Yemei', 'id'=>6, 'email'=>'yemei@client.com', 'sexe'=>'M'],
+            ['nom' => 'Mopao', 'id'=>7, 'email'=>'mopao@client.com', 'sexe'=>'M'],
+            ['nom' => 'Leila', 'id'=>8, 'email'=>'tino@client.com', 'sexe'=>'F']
+        ];
+
         $result = $n1 + $n2;
         return $this->render(':calculator:add.html.twig', [
             'n1' => $n1,
             'n2' => $n2,
-            'result' => $result
+            'result' => $result,
+            'ctrl' => $this,
+            'yesterday' => new \DateTime("today -1 day"),
+            'list' => [8,5,9,4,3,6,4],
+            'name' => 'mopao',
+            'clients' => $clients
             ]);
     }
 
